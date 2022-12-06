56db656a1d34bb625412f98e05350aad77214aaa
青椒 <johnyin@bu.edu>
2022-12-06 16:43:01 -0500

    add new red candles

--- SceneThree.py
+++ SceneThree.py
@@ -28,7 +28,6 @@ from DisplayableCube import DisplayableCube
 from DisplayableTorus import DisplayableTorus
 from DisplayableSphere import DisplayableSphere
 
-
 _material_donut = Material(np.array((0.1, 0.1, 0.1, 0.1)), np.array((0.4, 0.4, 0.4, 1)),
                            np.array((0.4, 0.4, 0.4, 0.1)), 64)
 
@@ -50,6 +49,10 @@ _material_candle = Material(np.array((0.1, 0.1, 0.1, 0.1)), np.array((0.4, 0.4, 
 def get_candle(shaderProg,
                pos=Point((0, 0, 0)),
                line_color: Ct.ColorType = Ct.WHITE,
+               height: float = 0.4,
+               thickness: float = 0.05,
+               nsides: int = 8,
+               candle_color: Ct.ColorType = Ct.WHITE,
                candle_texture: Optional[str] = "assets/white_candle.jpg",
                normal_texture: Optional[str] = "assets/white_candle_norm.png",
                use_texture: bool = True) -> Component:
@@ -57,8 +60,8 @@ def get_candle(shaderProg,
         shaderProg, 0.01, 0.01, 0.08, 3, line_color))
     candle_line.renderingRouting = "vertex"
     candle_line.setDefaultAngle(-90, candle_line.uAxis)
-    candle_base = Component(Point((0, 0, -(0.4 + 0.08) / 2)), DisplayableCylinder(
-        shaderProg, 0.05, 0.05, 0.4, 8, Ct.WHITE))
+    candle_base = Component(Point((0, 0, -(height + 0.08) / 2)), DisplayableCylinder(
+        shaderProg, thickness, thickness, height, nsides, candle_color))
     candle_base.setMaterial(_material_candle)
     if use_texture and candle_texture is not None:
         candle_base.setTexture(shaderProg, candle_texture)
@@ -137,18 +140,25 @@ class SceneThree(Scene):
         mat_plate = Material(np.array((0.1, 0.1, 0.1, 0.1)), np.array((0.5, 0.5, 0.5, 1)),
                              np.array((0.7, 0.7, 0.7, 0.1)), 16)
         plate1 = Component(Point((0.2, (0.5 + 0.05) / 2, 0)), DisplayableCylinder(
-            shaderProg, 0.9, 0.9, 0.05, 36, Ct.WHITE))
+            shaderProg, 0.9, 0.9, 0.05, 30, Ct.WHITE))
         plate1.setMaterial(mat_plate)
         plate1.setDefaultAngle(-90, plate1.uAxis)
         plate1.renderingRouting = "lighting"
         table.addChild(plate1)
 
         plate2 = Component(Point((-1.9, (0.5 + 0.05) / 2, 0.3)), DisplayableCylinder(
-            shaderProg, 1, 1, 0.05, 36, Ct.WHITE))
+            shaderProg, 1, 1, 0.05, 30, Ct.WHITE))
         plate2.setMaterial(mat_plate)
         plate2.setDefaultAngle(-90, plate2.uAxis)
         plate2.renderingRouting = "lighting"
         table.addChild(plate2)
+
+        plate3 = Component(Point((-2.4, (0.5 + 0.05) / 2, -1.1)), DisplayableCylinder(
+            shaderProg, 0.35, 0.35, 0.05, 20, Ct.WHITE))
+        plate3.setMaterial(mat_plate)
+        plate3.setDefaultAngle(-90, plate3.uAxis)
+        plate3.renderingRouting = "lighting"
+        table.addChild(plate3)
 
         pi = np.pi
         # add twenty donuts
@@ -230,3 +240,14 @@ class SceneThree(Scene):
             if i != 3:
                 light_helper(candle_light, candle, False)
             self.addChild(candle)
+
+        # add 1 red thick candle on plate3
+        l_pos = Point((-2.4, -0.2, -1.1))
+        red_candle = get_candle(shaderProg, l_pos, Ct.RED, height=0.5,
+                                thickness=0.15, nsides=24, candle_color=Ct.RED,
+                                candle_texture="assets/red_candle.png",
+                                normal_texture="assets/red_candle_norm.png")
+        candle_light = Light(l_pos, np.array((*Ct.RED, 1.0)))
+        self.lights.append(candle_light)
+        self.lightCubes.append(red_candle)
+        self.addChild(red_candle)
