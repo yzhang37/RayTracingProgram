36e178c734498e92e89e83b445c5cf28ac3da850
青椒 <johnyin@bu.edu>
2022-12-06 21:43:25 -0500

    set offsets

--- SceneFour.py
+++ SceneFour.py
@@ -41,6 +41,9 @@ class SceneFour(Scene):
         table.renderingRouting = "lighting_texture"
         self.addChild(table)
 
+        c_x = 0
+        c_y = 0
+        c_z = 0
         # add balls 1 - 15
         ball_radius = 0.2
         height = ball_radius * 2 * 4 * math.sqrt(3) / 2
@@ -49,8 +52,8 @@ class SceneFour(Scene):
             base_x = pa * height
             for j in range(0, i + 1):
                 pb = j - i / 2
-                base_z = pb * ball_radius * 2
-                ball = Component(Point((base_x, 0.5 / 2 + ball_radius, base_z)), DisplayableSphere(
+                base_z = -pb * ball_radius * 2
+                ball = Component(Point((c_x + base_x, c_y + 0.5 / 2 + ball_radius, c_z + base_z)), DisplayableSphere(
                     shaderProg, ball_radius, 16, 16, ColorType.WHITE
                 ))
                 ball_count += 1
@@ -58,12 +61,13 @@ class SceneFour(Scene):
                 print(filename)
                 ball.setTexture(shaderProg, filename)
                 ball.renderingRouting = "lighting_texture"
+                ball.setDefaultAngle(-90, ball.vAxis)
                 table.addChild(ball)
 
         # add the frame of the balls, it's radius should be 2/3 * height
         inner = height * 2/3 + ball_radius * 2
         thickness = 0.4
-        frame = Component(Point((0, (0.5 + thickness) / 2, 0)), DisplayableTorus(
+        frame = Component(Point((c_x, c_y + (0.5 + thickness) / 2, c_z)), DisplayableTorus(
             shaderProg, inner, inner + thickness, 3, 15, ColorType.ORANGE
         ))
         frame.setDefaultAngle(90, frame.uAxis)
